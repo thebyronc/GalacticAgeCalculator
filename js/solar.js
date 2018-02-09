2ca386596115393c027f6d206c1b356152b2a288
Byron Chang <thebyronc@gmail.com>
2018-02-09 14:29:41 -0800

    add passing test to compare date in seconds

--- js/solar.js
+++ js/solar.js
@@ -1,15 +1,23 @@
 export class Solar {
-  constructor(yearBorn, monthBorn, dayBorn) {
-    this.yearBorn = yearBorn;
-    this.monthBorn = monthBorn;
-    this.dayBorn = dayBorn;
+  constructor(year, month, day) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
   }
 
   convertYearToSeconds() {
     let date = new Date();
-    let years = date.getFullYear() - this.yearBorn;
-    let seconds = Date.UTC(1971)/1000;
-    return seconds * years;
+    let years = date.getFullYear() - this.year;
+    let seconds = Date.UTC(1970 + years)/1000;
+    return seconds;
+  }
+
+  compareDates(dateA, dateB) {
+    let date = new Date();
+    let dateASeconds = Date.UTC(dateA.year,dateA.month,dateA.day)/1000;
+    let dateBSeconds = Date.UTC(dateB.year,dateB.month,dateB.day)/1000;
+
+    return Math.abs(dateASeconds - dateBSeconds);
   }
   checkError() {
     $(".errorCode").text("Works");
--- spec/solar-spec.js
+++ spec/solar-spec.js
@@ -5,7 +5,13 @@ describe('Solar', function() {
   beforeEach(function() {
     solar = new Solar(1986, 6, 1);
   });
-  it('convertYearToSeconds age 32', function() {
-    expect(1009152000).toEqual(solar.convertYearToSeconds());
+  it('convertYearToSeconds', function() {
+    expect(1009843200).toEqual(solar.convertYearToSeconds());
+  });
+
+  it('compareDates', function() {
+    let dateA = new Solar(1986, 6, 1);
+    let dateb = new Solar(1986, 6, 1);
+    expect(0).toEqual(solar.compareDates(dateA,dateb));
   });
 });
